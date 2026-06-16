e7a1ca272ab79931f39b0922cef7106b00d93722
wdz <15349812798@163.com>
2026-06-16 22:53:40 +0800

    feat: scaffold workflow-engine-server Spring Boot module

---
+++ workflow-engine-server/src/main/java/com/github/wf/server/WorkflowServerApp.java
@@ -0,0 +1,11 @@
+package com.github.wf.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowServerApp.class, args);
+    }
+}
---
+++ workflow-engine-server/src/test/java/com/github/wf/server/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.github.wf.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+    @Test
+    void contextLoads() {}
+}
